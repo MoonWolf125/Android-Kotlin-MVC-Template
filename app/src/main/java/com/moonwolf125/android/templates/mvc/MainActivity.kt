a16b87bb444d5e3e1b009d9890b7a2c71318eb6d
Kyle <kdahn@bushelpowered.com>
2020-06-16 01:34:28 -0500

    Initial Project startup

---
+++ app/src/main/java/com/moonwolf125/android/templates/mvc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moonwolf125.android.templates.mvc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
